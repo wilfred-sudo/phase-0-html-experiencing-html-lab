500eb05174051002bfbf4fdc996ed5a6615bd2d8
wilfredkiprop <kipropwilfred90@gmail.com>
2025-02-20 15:32:11 +0300

    plant list

--- test/indexTest.js
+++ test/indexTest.js
@@ -5,7 +5,7 @@ const path = require("path");
 describe("my_list.html", () => {
   it("should be present", () => {
     const fileExists = fs.existsSync(
-      path.resolve(__dirname, "..", "my_list.html")
+      path.resolve(__dirname, "..","lab", "my_list.html")
     );
     const errorHint =
       "Make sure you created a file called 'my_list.html' in the lab directory with your HTML document in it!";
